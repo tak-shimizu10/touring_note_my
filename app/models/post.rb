1fb47488e2ca607af9a7eca760e6b48019d774b3
tak-shimizu10 <tak.shim10@gmail.com>
2020-07-01 11:39:11 +0900

    投稿内容を表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,8 @@ class Post < ApplicationRecord
   validates :tag_id, :title, :text, :image, presence: true, uniqueness: true
 
   belongs_to :user
+  belongs_to :tag
   has_many   :comments, dependent: :destroy
-  has_many   :tags, through: :post_tags
-  has_many   :post_tags, dependent: :destroy
 
   mount_uploader :image, ImageUploader
 end
